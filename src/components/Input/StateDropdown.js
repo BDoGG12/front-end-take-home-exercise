d770b3ade2a56820c2111c14b9c8ea5939b69e3c
Ben Do <bdo.appworkshop@gmail.com>
2022-10-05 21:46:01 -0500

    Changed to .js extension.

--- src/components/Input/StateDropdown.js
+++ src/components/Input/StateDropdown.js
@@ -3,20 +3,19 @@ import { useState } from 'react';
 
 const StateDropdown = (props) => {
 
-  const [value, setValue] = useState();
+  const [value, setValue] = useState('');
 
   const onValueChange = (e) => {
-    setValue(e.target.currentValue);
+    setValue(e.target.value);
     props.onStateClick(value)
   };
 
   return (
     <>
       <Form.Select aria-label={props.label} onChange={onValueChange}>
-        <option value=''>State</option>
-        {props.data.states.map((item) => (
-          <option value={item.abbreviation}>{item.abbreviation}</option>
-        ))}
+        <option value=''>{props.label}</option>
+        {props.data.map(item => (
+        <option value={item.abbreviation} key={item.abbreviation}>{item.abbreviation}</option>))}
       </Form.Select>
     </>
   )
