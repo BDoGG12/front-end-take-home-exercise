0044f8e8ec0570c11b4d1d546294615fe06c0ccb
Ben Do <bdo.appworkshop@gmail.com>
2022-10-05 21:44:06 -0500

    Created Home component and styling.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.module.css';
+
+const Home = () => {
+
+  return (
+    <div className='home'>
+      <header className='home-header'>
+        Fetch Rewards
+      </header>
+    </div>
+  )
+}
+
+export default Home;
