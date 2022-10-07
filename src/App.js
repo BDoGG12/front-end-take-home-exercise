b3368b74a0d8a057f4f3a450872ba41d27cabaf2
Ben Do <bdo.appworkshop@gmail.com>
2022-10-06 23:25:55 -0500

    Added Routes, Home, MenuBar components.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
 import UserForm from './components/UserForm/UserForm';
+import Home from './components/Home/Home';
+import MenuBar from './layout/MenuBar';
+import React, { useState, useRef, useEffect } from "react";
+import axios from 'axios';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
 
+  const url = 'https://frontend-take-home.fetchrewards.com/form';
+  const [formData, setFormData] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: url,
+      responseType: 'json'
+    })
+    .then((response) => {
+      setFormData(response.data)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        Fetch Rewards
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-up' element={<UserForm data={formData} />} />
+      </Routes>
+      <MenuBar />
     </div>
   );
 }
