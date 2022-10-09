cb88f46261aef30d7878aa3c8e03a93f8faa3dfa
Ben Do <bdo.appworkshop@gmail.com>
2022-10-08 22:49:51 -0500

    Added Welcome message and call to action.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import {Fragment} from 'react';
+import {Nav} from 'react-bootstrap';
 import './Home.module.css';
 
 const Home = () => {
 
   return (
     <Fragment>
+      <h3>Welcome To Fetch Rewards!</h3>
+      <p>Want to win reward points on shopping on your favorite brands?</p>
+      <p>Sign up <a href='/sign-up'>here</a>!</p>
     </Fragment>
-  )
-}
+  );
+};
 
 export default Home;
