cfdd66c2800b654bd6cb6b51f67da0f5d9fe0d77
Ben Do <bdo.appworkshop@gmail.com>
2022-10-08 22:54:05 -0500

    Removed className property from Nav.

--- src/layout/MenuBar.js
+++ src/layout/MenuBar.js
@@ -6,11 +6,10 @@ const MenuBar = () => {
 
   return (
     <Fragment>
-      <Navbar bg='light' variant='dark' expand='lg'>
+      <Navbar bg='dark' variant='dark'>
         <Container>
           <Navbar.Brand href='/'>Fetch Rewards</Navbar.Brand>
-          <Nav className={classes['menu']}>
-            <Nav.Link href='/'>Home</Nav.Link>
+          <Nav>
             <Nav.Link href='/sign-up'>Sign Up</Nav.Link>
           </Nav>
         </Container>
