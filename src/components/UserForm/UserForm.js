7273f205cf34dd2ac678148d2466172c35ea67d1
Ben Do <bdo.appworkshop@gmail.com>
2022-10-08 22:53:43 -0500

    Added validation constants, email, made button primary.

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,25 +1,33 @@
-import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
+import React, { useState, Fragment } from "react";
+import {Button, Col, Form, InputGroup, Row, Alert} from 'react-bootstrap';
 import UserInput from '../Input/UserInput';
 import OccupationDropdown from '../Input/OccupationDropdown';
 import StateDropdown from '../Input/StateDropdown';
+import axios from 'axios';
 
 
 const UserForm = (props) => {
 
+  // input values
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [state, setState] = useState('');
   const [occupation, setOccupation] = useState('');
+
+  // input validation
+  const [firstNameValidated, setFirstNameValidated] = useState(false);
+  const [lastNameValidated, setLastNameValidated] = useState(false);
+  const [emailValidated, setEmailValidated] = useState(false);
+  const [passwordValidated, setPasswordValidated] = useState(false);
+  const [stateValidated, setStateValidated] = useState(false);
+  const [occupationValidated, setOccupationValidated] = useState(false);
   const [validated, setValidated] = useState(false);
 
   const firstNameLabel = 'First Name';
   const lastNameLabel = 'Last Name';
+  const emailLabel = 'Email'
   const passwordLabel = 'Password'
   const occupationLabel = 'Occupation';
   const stateLabel = 'State';
@@ -30,6 +38,10 @@ const UserForm = (props) => {
 
   const onLastNameChange = (lastName) => {
     setLastName(lastName);
+  };
+
+  const onEmailChange = (email) => {
+    setEmail(email);
   };
 
   const onPasswordChange = (password) => {
@@ -44,27 +56,73 @@ const UserForm = (props) => {
     setOccupation(occupation);
   };
 
-
-
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    const form = e.currentTarget;
+    if (firstName === '' && lastName === '' && email === '' && password === '' && occupation === '' & state === '') {
+      setValidated(false);
+      setFirstNameValidated(false);
+      setLastNameValidated(false);
+      setEmailValidated(false);
+      setPasswordValidated(false);
+      setOccupationValidated(false);
+      setStateValidated(false);
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('could not send post request');
+    } else {
+      console.log('sent post request');
+      setValidated(true);
+      axios({
+        method: 'post',
+        url: props.url,
+        data: {
+          name: `${firstName} ${lastName}`,
+          email: email,
+          password: password,
+          occupation: occupation,
+          state: state
+        }
+      })
+    }
 
   };
 
   return (
-    <div>
+    <Fragment>
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row className='name-password'>
-      <UserInput label={firstNameLabel} type='text' onInputChange={onFirstNameChange} />
-      <UserInput label={lastNameLabel} type='text' onInputChange={onLastNameChange} />
-      <UserInput label={passwordLabel} type='password' onInputChange={onPasswordChange} />
-      </Row>
-      <Row>
-        <OccupationDropdown label={occupationLabel} onOccupationClick={onOccupationChange} data={props.data.occupations} />
-        <StateDropdown label={stateLabel} onStateClick={onStateChange} data={props.data.states} />
-      </Row>
-      <Button type='submit'>Submit</Button>
-    </Form>
-    </div>
+        <Row className='name-password'>
+          <UserInput
+            label={firstNameLabel}
+            type='text'
+            onInputChange={onFirstNameChange} />
+          <UserInput
+            label={lastNameLabel}
+            type='text'
+            onInputChange={onLastNameChange} />
+          <UserInput
+            label={emailLabel}
+            type='email'
+            onInputChange={onEmailChange} />
+          <UserInput
+            label={passwordLabel}
+            type='password'
+            onInputChange={onPasswordChange} />
+        </Row>
+        <Row>
+          <OccupationDropdown
+            label={occupationLabel}
+            onOccupationClick={onOccupationChange}
+            data={props.data.occupations} />
+          <StateDropdown
+            label={stateLabel}
+            onStateClick={onStateChange}
+            data={props.data.states} />
+        </Row>
+        <Button
+          variant='primary'
+          type='submit'>Submit</Button>
+      </Form>
+    </Fragment>
   );
 };
 
