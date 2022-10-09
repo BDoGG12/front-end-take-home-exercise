ae22bb5783c056b0fbc1b2d2f8ebdd3fbb598061
Ben Do <bdo.appworkshop@gmail.com>
2022-10-08 22:52:14 -0500

    Imported Fragment and classes, render feedback.

--- src/components/Input/UserInput.js
+++ src/components/Input/UserInput.js
@@ -1,5 +1,6 @@
-import {Form, InputGroup} from 'react-bootstrap';
-
+import { Form, InputGroup } from 'react-bootstrap';
+import classes from './UserInput.module.css';
+import { Fragment } from 'react';
 
 const UserInput = (props) => {
 
@@ -8,15 +9,16 @@ const UserInput = (props) => {
   };
 
   return (
-    <>
-    <Form.Label>{props.label}</Form.Label>
-    <Form.Control
-      required
-      type={props.type}
-      placeholder={props.label}
-      onChange={onValueChange}
+    <Fragment>
+      <Form.Label>{props.label}</Form.Label>
+      <Form.Control
+        required
+        type={props.type}
+        placeholder={props.label}
+        onChange={onValueChange}
       />
-    </>
+      {!props.validate ? <Form.Control.Feedback>Looks good!</Form.Control.Feedback> : <Form.Control.Feedback type='invalid'>Please provide a {props.label}</Form.Control.Feedback>}
+    </Fragment>
   );
 };
 
